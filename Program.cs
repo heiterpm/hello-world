d70c1ea790cef3bafab18d11376e0873438091be
Heiter Paganin Malagoli <heiterpm@gmail.com>
2021-08-06 17:55:54 -0300

    Alterado para Fatec

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Olá Fatec!");
         }
     }
 }
